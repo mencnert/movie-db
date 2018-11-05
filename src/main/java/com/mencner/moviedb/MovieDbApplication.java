6ab76dada9c6274f9810ef43c63d368c8bd12e5c
meny <mencnert@gmail.com>
2018-11-05 17:34:25 +0100

    initial commit

---
+++ src/main/java/com/mencner/moviedb/MovieDbApplication.java
@@ -0,0 +1,12 @@
+package com.mencner.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDbApplication.class, args);
+	}
+}
